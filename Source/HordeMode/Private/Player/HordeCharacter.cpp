0195d044cad353a598862c0f8051974b88e0d55c
Jordan-Blackett <Jordan.blackett@gmail.com>
2019-01-17 12:40:30 +0000

    Player Character Setup

--- Source/HordeMode/Private/Player/HordeCharacter.cpp
+++ Source/HordeMode/Private/Player/HordeCharacter.cpp
@@ -1,7 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "HordeCharacter.h"
-
+#include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/PawnMovementComponent.h"
 
 // Sets default values
 AHordeCharacter::AHordeCharacter()
@@ -9,20 +12,20 @@ AHordeCharacter::AHordeCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArmComp->bUsePawnControlRotation = true;
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->SetupAttachment(SpringArmComp);
+
+	GetMovementComponent()->GetNavAgentPropertiesRef().bCanCrouch = true;
 }
 
 // Called when the game starts or when spawned
 void AHordeCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AHordeCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
@@ -30,5 +33,40 @@ void AHordeCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &AHordeCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AHordeCharacter::MoveRight);
+
+	PlayerInputComponent->BindAxis("LookUp", this, &AHordeCharacter::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("Turn", this, &AHordeCharacter::AddControllerYawInput);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &AHordeCharacter::BeginCrouch);
+	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &AHordeCharacter::EndCrouch);
 }
 
+void AHordeCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector() * Value);
+}
+
+void AHordeCharacter::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector() * Value);
+}
+
+void AHordeCharacter::BeginCrouch()
+{
+	Crouch();
+}
+
+void AHordeCharacter::EndCrouch()
+{
+	UnCrouch();
+}
+
+// Called every frame
+void AHordeCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
--- Source/HordeMode/Public/Player/HordeCharacter.h
+++ Source/HordeMode/Public/Player/HordeCharacter.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Character.h"
 #include "HordeCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class HORDEMODE_API AHordeCharacter : public ACharacter
 {
@@ -19,6 +22,20 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
+	void BeginCrouch();
+
+	void EndCrouch();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UCameraComponent* CameraComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USpringArmComponent* SpringArmComp;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -26,6 +43,4 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
-	
 };
